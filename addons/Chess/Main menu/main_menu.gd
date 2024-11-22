73935ed839d2f5d1cd4088b615f7f634aab4f6f9
perrobot64-developer64 <138079625+perrobot64-developer64@users.noreply.github.com>
2024-11-21 23:44:52 -0300

    casi termina

---
+++ addons/Chess/Main menu/main_menu.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_jugar_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_creditos_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_salir_pressed() -> void:
+	pass # Replace with function body.
